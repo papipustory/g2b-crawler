b25b9c310fd57a96b96ca99af1d5436cd67d40c8
papipustory <hohomax88@gmail.com>
2025-08-04 15:14:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
+
 #!/bin/bash
-
-# Streamlit Cloud가 빌드 단계에서 이 스크립트를 실행합니다.
-# Playwright가 사용하는 브라우저 바이너리를 설치합니다.
-# 여기서는 크롤러에 필요한 Chromium만 설치합니다.
-
-playwright install --with-deps
+# Playwright 브라우저 설치
+echo "▶ Installing Playwright Chromium..."
+python3 -m playwright install chromium
