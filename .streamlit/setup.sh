4758a2ef080c4c963639dc2b4e9a9820de262d8b
papipustory <hohomax88@gmail.com>
2025-08-04 15:31:20 +0900

    Update setup.sh

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+apt-get update
+apt-get install -y wget unzip curl
+
+# 필요한 시스템 패키지 설치
+xargs -r -a packages.txt apt-get install -y
+
+# Python 패키지 설치
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Playwright 브라우저 설치
+playwright install chromium
