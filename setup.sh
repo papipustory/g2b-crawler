5b2c048bd7891b23c4bc101a5c4cbedd13dd9d49
papipustory <hohomax88@gmail.com>
2025-08-04 15:22:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+apt-get update
+apt-get install -y wget unzip curl
 
-apt-get update && apt-get install -y \
-  libnss3 libnspr4 libatk-bridge2.0-0 libdrm2 libgtk-3-0 libgbm1 \
-  libasound2 libxss1 libxrandr2 libu2f-udev libvulkan1 xvfb \
-  fonts-liberation libappindicator3-1 libgconf-2-4 libatk1.0-0 \
-  libcups2 libpango-1.0-0 libcairo2 libdbus-1-3 wget curl unzip \
-  python3-pip
+# 필요한 시스템 패키지 설치
+xargs -r -a packages.txt apt-get install -y
 
+# Python 패키지 설치
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# 반드시 chromium만 설치
+# Playwright 브라우저 설치
 playwright install chromium
