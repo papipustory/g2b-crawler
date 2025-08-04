bad3c30bef3c30459809ff6035c3499adddb7786
papipustory <hohomax88@gmail.com>
2025-08-04 15:17:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,34 @@
-
 #!/bin/bash
-# Playwright 브라우저 설치
-echo "▶ Installing Playwright Chromium..."
-python3 -m playwright install chromium
+
+# 시스템 패키지 설치
+apt-get update && apt-get install -y \
+  libnss3 \
+  libnspr4 \
+  libatk-bridge2.0-0 \
+  libdrm2 \
+  libgtk-3-0 \
+  libgbm1 \
+  libasound2 \
+  libxss1 \
+  libxrandr2 \
+  libu2f-udev \
+  libvulkan1 \
+  xvfb \
+  fonts-liberation \
+  libappindicator3-1 \
+  libgconf-2-4 \
+  libatk1.0-0 \
+  libcups2 \
+  libpango-1.0-0 \
+  libcairo2 \
+  libdbus-1-3 \
+  wget \
+  curl \
+  unzip \
+  python3-pip
+
+# Python 패키지 설치
+pip install -r requirements.txt
+
+# [🔥중요] playwright 브라우저 설치
+playwright install --with-deps
