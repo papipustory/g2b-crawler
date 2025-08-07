729feb2c73c14f84a441062d26addf71fe2884fb
papipustory <hohomax88@gmail.com>
2025-08-07 10:58:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Playwright 브라우저 설치
+# install-deps는 packages.txt로 대체되므로, 브라우저만 설치합니다.
+set -e
+echo "--- Installing Playwright browser ---"
 python -m playwright install chromium
-python -m playwright install-deps chromium
+echo "--- Playwright browser installation finished ---"
